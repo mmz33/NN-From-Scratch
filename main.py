420d270ac62f316062e377383ea504ddaa697ae5
Mohammad Zeineldeen <mohammad.zeineldeen@rwth-aachen.de>
2019-04-30 20:49:45 +0200

    added hyperparam as args, move save/load model to utils

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import nn_module
 import train
 import test
 import argparse
-import pickle
+import utils
 import sys
 
 
 # TODO: make this more flexible, such as parsing a json config
 # just a simple example demo
-def build_and_train_model(batch_size, epochs, lr, train_data, valid_data):
+def build_and_train_model(batch_size, epochs, lr, start_epoch_decay, decay_rate, train_data, valid_data):
     modules_list = [
         (nn_module.Linear, {'n_in': 28*28, 'n_out': 200}),
         (nn_module.Tanh, {}),
@@ -20,14 +20,13 @@ def build_and_train_model(batch_size, epochs, lr, train_data, valid_data):
 
     net_model = model.NNModel(modules_list)
     loss = nn_module.CrossEntropyLoss()
-    train.train(net_model, loss, batch_size, epochs, lr, train_data, valid_data)
+    train.train(net_model, loss, batch_size, epochs, lr, start_epoch_decay, decay_rate, train_data, valid_data)
     return net_model
 
 
 def main():
     """Main entry point function. This is where it begins"""
 
-    # TODO: add hyperparams as args
     parser = argparse.ArgumentParser()
     parser.register('type', 'bool', lambda v: v.lower() == 'true')
     parser.add_argument("--model_file", type=str, default='./model.pkl', help='Pickle file for model (dir has to exist)')
@@ -35,6 +34,13 @@ def main():
     parser.add_argument('--train', type='bool', nargs='?', const=True, default=False, help='Run training')
     parser.add_argument('--test', type='bool', nargs='?', const=True, default=False, help='Run test')
     parser.add_argument('--log_file', type=str, default=None, help='log file dir (dir has to exist)')
+
+    # hyperparameters
+    parser.add_argument('--lr', type=float, default=0.7, help='initial learning rate')
+    parser.add_argument('--num_epochs', type=int, default=10, help='number of training epochs')
+    parser.add_argument('--batch_size', type=int, default=100, help='minibatch size')
+    parser.add_argument('--decay_rate', type=float, default=0.3, help='the decay of the learning rate')
+    parser.add_argument('--start_epoch_decay', type=float, default=5, help='start lr decay epoch')
 
     args = parser.parse_args()
 
@@ -47,19 +53,22 @@ def main():
 
     net_model = None
     if args.train:
-        net_model = build_and_train_model(1000, 20, 0.7, datasets.train, datasets.valid)
+        net_model = build_and_train_model(args.batch_size,
+                                          args.num_epochs,
+                                          args.lr,
+                                          args.start_epoch_decay,
+                                          args.decay_rate,
+                                          datasets.train,
+                                          datasets.valid)
         # save model for testing later
         if net_model and args.model_file:
-            print('Saving model to file %s' % args.model_file)
-            with open(args.model_file, 'wb') as f:
-                pickle.dump(net_model, f)
+            utils.save_model(args.model_file, net_model)
 
     if args.test:
         # in case we called test later so we need to check if there exists an already trained model
         if not net_model:
             if args.model_file:
-                with open(args.model_file, 'rb') as f:
-                    net_model = pickle.load(f)
+                net_model = utils.load_model(args.model_file)
             else:
                 raise Exception('No trained model for testing')
 
