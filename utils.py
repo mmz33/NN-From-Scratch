454b41af98f4627849368b03ab4328c243d1309a
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2020-02-08 19:34:39 +0100

    handle case when model dir doesn't exist

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 
 
 def save_model(model_file, net_model):
@@ -8,7 +9,9 @@ def save_model(model_file, net_model):
     :param model_file: A string, model file string path
     :param net_model: A model, trained network model (learned params)
     """
-    print('Saving model to file %s' % model_file)
+    print('Saving model to %s' % model_file)
+    if '/' in model_file:
+        os.makedirs(os.path.dirname(model_file), exist_ok=True)
     with open(model_file, 'wb') as f:
         pickle.dump(net_model, f)
 
